ea2b313f47aaa5931b424745aa9afaa008c3d9e5
Ellis Brown <ellis.brown@tufts.edu>
2023-03-20 04:06:41 -0400

    Change function signature

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,5 +1,7 @@
 import { OpenAIStream, OpenAIStreamPayload, ChatGPTMessage } from "./stream_parsing";
+import {NextRequest, NextFetchEvent} from 'next/server'
 // Setting this runtime from nodejs to edge causes the app to fail in prod
+
 export const config = {
   runtime: "edge",
 };
@@ -7,8 +9,8 @@ export type InputMessage = {
   text: string;
   isUser: boolean;
 }
-export async function POST(request: Request, response: Response) {  
-  const { messages } : {  messages: InputMessage[] } = await request.json(); //  { messages: InputMessage[] 
+export default async function POST(request: NextRequest, context: NextFetchEvent) {  
+  const { messages } : {  messages: InputMessage[] } = await request.json();  
 
   const transformedMessages: ChatGPTMessage[] = messages.map((msg: { isUser: any; text: any; }) => ({
     role: msg.isUser ? "user" : "system",
