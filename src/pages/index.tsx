647512fb6710ba8bbac6e299addda96971c75571
Ellis Brown <ellis.brown@tufts.edu>
2023-03-25 23:13:43 -0400

    Change welcome message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,5 +2,5 @@ import ChatApplication from '@/components/ChatApplication';
 
 export default function HomePage() {
     
-    return (<><h1 className="bg-yellow-600 p-4 rounded-md m-4">Currently restricted to users with the passcode. Text me and donate on venmo @ellis434890, I will give you a passcode </h1><ChatApplication /></>)
+    return (<><h1 className="bg-yellow-600 p-4 rounded-md m-4">Currently restricted to users with the passcode. Ask me for passcode if needed</h1><ChatApplication /></>)
 }
